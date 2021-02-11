d91c745914b6d2a27f5ddbd624c6ff85d0821094
Alex Fuller <fulleralex47@gmail.com>
2021-02-11 08:07:27 -0500

    Complete tutorial

---
+++ scripts/app.ts
@@ -0,0 +1,6 @@
+﻿function SayHello() {
+    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
+    const framework = (document.getElementById("framework") as HTMLInputElement).value;
+
+    return `Hello from ${compiler} and ${framework}`;
+}
---
+++ wwwroot/scripts/app.js
@@ -0,0 +1,6 @@
+function SayHello() {
+    const compiler = document.getElementById("compiler").value;
+    const framework = document.getElementById("framework").value;
+    return `Hello from ${compiler} and ${framework}`;
+}
+//# sourceMappingURL=app.js.map
